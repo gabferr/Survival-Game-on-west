ac276ea4f648fdef9605d13b7bbb8002b6672a0b
gabferr <gabrielferrandin@hotmail.com>
2025-10-29 18:36:37 -0300

    Comit inicial

---
+++ Source/OldWestLearning/Public/Combat/WeaponTypes.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "WeaponTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponTypes : uint8
+{
+	EWT_NoWeapon UMETA(DisplayName = "NoWeapon"),
+	EWT_Pistol UMETA(DisplayName = "Pistol"),
+	EWT_Rifle UMETA(DisplayName = "Rifle"),
+	EWT_Shotgun UMETA(DisplayName = "Shotgun"),
+	EWT_Grenade UMETA(DisplayName = "Grenade Launcher"),
+	EWT_Melee UMETA(DisplayName = "Melee"),
+	EWT_MAX UMETA(Hidden)
+};
